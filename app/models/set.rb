3291170040f7dfea3f1206598f445700069a6f50
Norman Sue <normful@gmail.com>
2014-07-31 11:05:50 -0700

    fix validations

--- app/models/set.rb
+++ app/models/set.rb
@@ -2,9 +2,9 @@ class Set < ActiveRecord::Base
 
   has_many :tracks
 
-  validates: artist, presence: true
-  validates: title, presence: true
-  validates: filepath, presence: true
-  validates: date_aired, date: true
+  validates :artist, presence: true
+  validates :title, presence: true
+  validates :filepath, presence: true, uniqueness: true
+  validates :date_aired, date: true
 
 end
--- app/models/track.rb
+++ app/models/track.rb
@@ -2,9 +2,9 @@ class Track < ActiveRecord::Base
 
   belongs_to :set
 
-  validates: artist, presence: true
-  validates: title, presence: true
-  validates: filepath, presence: true
-  validates: track_number, numericality { greater_than: 0, only_integer: true }
+  validates :artist, presence: true
+  validates :title, presence: true
+  validates :filepath, presence: true, uniqueness: true
+  validates :track_number, numericality: { greater_than: 0, only_integer: true }
 
 end
