2b8715d61f9e00e24767f541d77a017b14619933
Norman Sue <normful@gmail.com>
2014-07-31 18:04:35 -0700

    fix one-to-many association

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,10 +1,9 @@
 module Music
   class Track < ActiveRecord::Base
-
+    belongs_to :set
     validates :artist, presence: true
     validates :title, presence: true
     validates :filepath, presence: true, uniqueness: true
     validates :track_number, numericality: { greater_than: 0, only_integer: true }
-
   end
 end
