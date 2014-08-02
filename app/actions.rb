2f3981d1b3af8c66c064b7c66b223a440564f818
Norman Sue <normful@gmail.com>
2014-08-01 19:13:46 -0700

    add actions.rb draft

--- app/actions.rb
+++ app/actions.rb
@@ -1,15 +1,15 @@
 get '/' do
   erb :home
+  @livesets = Liveset.all
 end
 
-post '/' do
-  redirect '/search'
-end
-
-get '/search' do
+post '/search' do
+  search = params[:query]
+  @livesets = Liveset.where("artist LIKE ? OR title LIKE ?", "%#{search}%","%#{search}%","%#{search}%")
   erb :search
 end
 
 get '/set/:id' do
+  @liveset = Liveset.find(params[:id])
   erb :'set/show'
 end
