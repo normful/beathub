8551b53237458f6216d6d2baa1e930a03a3e1392
Norman Sue <normful@gmail.com>
2014-08-01 20:09:18 -0700

    add embedded ruby in search.erb

--- app/actions.rb
+++ app/actions.rb
@@ -5,7 +5,8 @@ end
 
 post '/search' do
   search = params[:query]
-  @livesets = Liveset.where("artist LIKE ? OR title LIKE ?", "%#{search}%","%#{search}%","%#{search}%").order(date_aired: :desc)
+  @livesets = Liveset.where("artist LIKE ? OR title LIKE ?", "%#{search}%","%#{search}%").order(date_aired: :desc)
+  # TODO: Also search through tracks that have the search query
   erb :search
 end
 
