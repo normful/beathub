10bb43a7bd5d17184dd0cc359e27f78dd2bbfdd3
Norman Sue <normful@gmail.com>
2014-08-01 20:13:17 -0700

    change route pattern to allow optional last forward slash

--- app/actions.rb
+++ app/actions.rb
@@ -3,14 +3,14 @@ get '/' do
   erb :home
 end
 
-post '/search' do
+get '/search/?' do
   search = params[:query]
   @livesets = Liveset.where("artist LIKE ? OR title LIKE ?", "%#{search}%","%#{search}%").order(date_aired: :desc)
   # TODO: Also search through tracks that have the search query
   erb :search
 end
 
-get '/liveset/:id' do
+get '/liveset/:id/?' do
   @liveset = Liveset.find(params[:id])
   erb :'liveset/show'
 end
