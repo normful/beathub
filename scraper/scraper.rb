8951315226229ba96d75674f52b5f372fee55134
Norman Sue <normful@gmail.com>
2014-08-03 00:12:08 -0700

    fix puts statements

--- scraper/scraper.rb
+++ scraper/scraper.rb
@@ -18,12 +18,12 @@ class Scraper
     @agent.page.links_with(href: /^.+\.shtml$/, text: /^.+\s-\s.+$/).first(2).each do |link|
       liveset_page = link.click
       mp3_link = liveset_page.link_with(href: /^.*livesets.us\/d\;.*\.mp3$/)
-      puts "Downloading #{mp3_link}"
       mp3_file = mp3_link.click
       mp3_filename = mp3_file.filename
+      puts "Downloading #{mp3_filename}"
       mp3_folder = "#{MUSIC_FOLDER}/#{mp3_file.filename.chomp(".mp3")}"
       mp3_file.save "#{mp3_folder}/#{mp3_filename}"
-      puts "Saving in #{mp3_folder}/#{mp3_filename}"
+      puts "Saving in ./#{MUSIC_FOLDER}/#{mp3_folder}/#{mp3_filename}"
       begin
         save_exact_matching_cue(mp3_filename, mp3_folder)
       rescue StandardError => e
@@ -38,10 +38,10 @@ class Scraper
     @agent.page.forms[1]["filename"] = query
     @agent.page.forms[1].submit
     cue_link = @agent.page.link_with(href: /^download\.php\?type=cue.*.cue$/)
-    puts "Downloading #{cue_link}"
     cue_file = cue_link.click # downloads the file
+    puts "Downloading #{cue_file.filename}"
     cue_file.save "#{folder}/#{cue_file.filename}"
-    puts "Saving in #{folder}/#{cue_file.filename}"
+    puts "Saving in ./#{MUSIC_FOLDER}/#{folder}/#{cue_file.filename}"
   end
 
   def save_best_matching_cue(query, folder)
