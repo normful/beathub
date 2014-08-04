971c725ac68ac87aece35a0310df11d8ff38a56e
Norman Sue <normful@gmail.com>
2014-08-03 23:35:34 -0700

    refactor zipper

--- scraper/zipper.rb
+++ scraper/zipper.rb
@@ -1,35 +1,39 @@
 require 'rubygems'
 require 'zip'
 
-def walk(start)
-  Dir.foreach(start) do |x|
-    path = File.join(start, x)
-    if x == "." or x == ".."
-      next
-    elsif File.directory?(path)
-      walk(path)
-    else
-      if x =~ /\.cue/
-        this_folder = File.dirname(path)
-        zip_file = "#{x.chomp(".cue")}.zip"
-        begin
-          zip_folder("#{this_folder}/split/", "#{this_folder}/#{zip_file}")
-        rescue Zip::EntryExistsError
-          puts "#{zip_file} already exists. Skipping."
+class Zipper
+
+  def walk(start)
+    Dir.foreach(start) do |x|
+      path = File.join(start, x)
+      if x == "." or x == ".."
+        next
+      elsif File.directory?(path)
+        walk(path)
+      else
+        if x =~ /\.cue/
+          this_folder = File.dirname(path)
+          zip_file = "#{x.chomp(".cue")}.zip"
+          begin
+            zip_folder("#{this_folder}/split/", "#{this_folder}/#{zip_file}")
+          rescue Zip::EntryExistsError
+            puts "#{zip_file} already exists. Skipping."
+          end
         end
       end
     end
   end
-end
 
-def zip_folder(folder, zip)
-  puts "Creating #{zip}"
-  Zip::File.open(zip, Zip::File::CREATE) do |zipfile|
-    Dir[File.join(folder, '**', '**')].sort.each do |file|
-      puts "Adding #{file}"
-      zipfile.add(file.sub(folder, ''), file)
+  def zip_folder(folder, zip)
+    puts "Creating #{zip}"
+    Zip::File.open(zip, Zip::File::CREATE) do |zipfile|
+      Dir[File.join(folder, '**', '**')].sort.each do |file|
+        puts "Adding #{file}"
+        zipfile.add(file.sub(folder, ''), file)
+      end
     end
   end
+
 end
 
-walk(Dir.pwd)
+Zipper.new.walk(Dir.pwd)
